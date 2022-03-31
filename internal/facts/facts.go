76319d307eacb3bc6e08e155211ab4e77e1ad1c3
Aaron Batilo <AaronBatilo@gmail.com>
2022-03-31 13:04:34 -0600

    Update facts.go

--- internal/facts/facts.go
+++ internal/facts/facts.go
@@ -190,7 +190,7 @@ func GenerateFact(id uint) (string, bool) {
 	completionRequest := completionRequest{
 		User:      user,
 		MaxTokens: 60,
-		Prompt:    "Tell me a cat joke",
+		Prompt:    "write a story about a cat",
 	}
 
 	jsonBody, err := json.Marshal(completionRequest)
